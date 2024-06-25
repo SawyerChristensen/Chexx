408b9de7483858e1dc8a7b17b64ddba9e5b2e84b
SawyerChristensen <schr5498@gmail.com>
2024-06-24 17:34:13 -0700

    working honeycomb pattern grid thing

--- Chexx/GameScene.swift
+++ Chexx/GameScene.swift
@@ -8,6 +8,35 @@
 import SpriteKit
 import GameplayKit
 
+class HexagonNode: SKShapeNode {
+    init(size: CGFloat) {
+        super.init()
+        
+        let path = UIBezierPath()
+        let angle: CGFloat = .pi / 3
+        for i in 0..<6 {
+            let x = size * cos(angle * CGFloat(i))
+            let y = size * sin(angle * CGFloat(i))
+            if i == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        path.close()
+        
+        self.path = path.cgPath
+        self.fillColor = .cyan
+        self.strokeColor = .black
+        self.lineWidth = 2
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
 class GameScene: SKScene {
     
     var entities = [GKEntity]()
@@ -16,6 +45,8 @@ class GameScene: SKScene {
     private var lastUpdateTime : TimeInterval = 0
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    
+    let hexagonSize: CGFloat = 50
     
     override func sceneDidLoad() {
 
@@ -27,6 +58,9 @@ class GameScene: SKScene {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
         }
+        
+        // Create hexagon grid
+        createHexagonGrid(rows: 10, columns: 10)
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
@@ -42,12 +76,32 @@ class GameScene: SKScene {
         }
     }
     
+    func createHexagonGrid(rows: Int, columns: Int) {
+            for row in 0..<rows {
+                for col in 0..<columns {
+                    let hexagon = HexagonNode(size: hexagonSize)
+                    let xOffset = hexagonSize * 1.5 * CGFloat(col)
+                    let yOffset = hexagonSize * sqrt(3) * CGFloat(row) + (col % 2 == 0 ? 0 : hexagonSize * sqrt(3) / 2)
+                    hexagon.position = CGPoint(x: xOffset, y: yOffset)
+                    hexagon.name = "hex_\(row)_\(col)"
+                    addChild(hexagon)
+                }
+            }
+        }
+    
     
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
             n.strokeColor = SKColor.green
             self.addChild(n)
+        }
+        let nodesAtPoint = nodes(at: pos)
+        for node in nodesAtPoint {
+            if let hexagon = node as? HexagonNode {
+                hexagon.fillColor = .yellow  // Example interaction: change color
+                print("Hexagon touched: \(hexagon.name ?? "unknown")")
+            }
         }
     }
     
