5638d4e456f59fc44c219d9d66788837bbb8482a
SawyerChristensen <schr5498@gmail.com>
2024-06-24 18:56:14 -0700

    blue tiles work ig

--- Chexx/GameScene.swift
+++ Chexx/GameScene.swift
@@ -38,65 +38,127 @@ class HexagonNode: SKShapeNode {
 
 
 class GameScene: SKScene {
-
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    let hexagonSize: CGFloat = 50
+    
     override func sceneDidLoad() {
         super.sceneDidLoad()
+
+        self.lastUpdateTime = 0
         
-        let rows = 11
-        let columns = 12
-        let hexSize: CGFloat = 30.0 // Adjust size as needed
+        // Get label node from scene and store it for use later
+        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        if let label = self.label {
+            label.alpha = 0.0
+            label.run(SKAction.fadeIn(withDuration: 2.0))
+        }
         
-        createHexagonGrid(rows: rows, columns: columns, hexSize: hexSize)
+        // Create hexagon grid
+        createHexagonGrid(rows: 10, columns: 10)
+        
+        // Create shape node to use during mouse interaction
+        let w = (self.size.width + self.size.height) * 0.05
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        
+        if let spinnyNode = self.spinnyNode {
+            spinnyNode.lineWidth = 2.5
+            
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+                                              SKAction.fadeOut(withDuration: 0.5),
+                                              SKAction.removeFromParent()]))
+        }
     }
     
-    func drawHexagon(at position: CGPoint, size: CGFloat) -> SKShapeNode {
-        let hexagonPath = UIBezierPath()
-        let width = size * sqrt(3)
-        let height = size * 2
-        let center = CGPoint(x: width / 2, y: height / 2)
-        
-        hexagonPath.move(to: CGPoint(x: center.x + width / 2, y: center.y))
-        hexagonPath.addLine(to: CGPoint(x: center.x + width / 4, y: center.y + height / 2))
-        hexagonPath.addLine(to: CGPoint(x: center.x - width / 4, y: center.y + height / 2))
-        hexagonPath.addLine(to: CGPoint(x: center.x - width / 2, y: center.y))
-        hexagonPath.addLine(to: CGPoint(x: center.x - width / 4, y: center.y - height / 2))
-        hexagonPath.addLine(to: CGPoint(x: center.x + width / 4, y: center.y - height / 2))
-        hexagonPath.close()
-        
-        let hexagon = SKShapeNode(path: hexagonPath.cgPath)
-        hexagon.position = position
-        hexagon.lineWidth = 2.0
-        hexagon.fillColor = SKColor.brown
-        hexagon.strokeColor = SKColor.black
-        
-        return hexagon
-    }
-    
-    func createHexagonGrid(rows: Int, columns: Int, hexSize: CGFloat) {
-        let hexWidth = hexSize * sqrt(3)
-        let hexHeight = hexSize * 2
-        
-        let centerX = CGFloat(columns / 2) * hexWidth
-        let centerY = CGFloat(rows / 2) * (hexHeight * 3 / 4)
-        
-        for row in 0..<rows {
-            for column in 0..<columns {
-                let xOffset = (CGFloat(column) * hexWidth) + ((row % 2 == 0) ? 0 : hexWidth / 2)
-                let yOffset = CGFloat(row) * (hexHeight * 3 / 4)
-                let xPos = centerX + xOffset
-                let yPos = centerY + yOffset
-                
-                // Center the grid around tile f6
-                if row == 5 && column == 5 {
-                    // Center of the grid (f6)
-                    hexagon.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-                } else {
-                    hexagon.position = CGPoint(x: xPos, y: yPos)
+    func createHexagonGrid(rows: Int, columns: Int) {
+            for row in 0..<rows {
+                for col in 0..<columns {
+                    let hexagon = HexagonNode(size: hexagonSize)
+                    let xOffset = hexagonSize * 1.5 * CGFloat(col)
+                    let yOffset = hexagonSize * sqrt(3) * CGFloat(row) + (col % 2 == 0 ? 0 : hexagonSize * sqrt(3) / 2)
+                    hexagon.position = CGPoint(x: xOffset, y: yOffset)
+                    hexagon.name = "hex_\(row)_\(col)"
+                    addChild(hexagon)
                 }
-                
-                let hexagon = drawHexagon(at: CGPoint(x: xPos, y: yPos), size: hexSize)
-                self.addChild(hexagon)
             }
         }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.green
+            self.addChild(n)
+        }
+        let nodesAtPoint = nodes(at: pos)
+        for node in nodesAtPoint {
+            if let hexagon = node as? HexagonNode {
+                hexagon.fillColor = .yellow  // Example interaction: change color
+                print("Hexagon touched: \(hexagon.name ?? "unknown")")
+            }
+        }
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.blue
+            self.addChild(n)
+        }
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position = pos
+            n.strokeColor = SKColor.red
+            self.addChild(n)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let label = self.label {
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        }
+        
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
     }
 }
